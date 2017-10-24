e94baa5f746c2c7a1d06fc96545c5ba691c079d5
José Lima <jose.slneto@gmail.com>
2017-10-24 16:08:42 -0200

    Docker (#2)
    * Added docker configuration and documentation.
    
    * Javadoc to DockerConfiguration class and remove unnecessary code on APIx.

--- src/main/java/com/github/groovylabs/lyre/config/DockerConfiguration.java
+++ src/main/java/com/github/groovylabs/lyre/config/DockerConfiguration.java
@@ -23,59 +23,38 @@
  *
  */
 
-package com.github.groovylabs.lyre;
+package com.github.groovylabs.lyre.config;
 
-import com.github.groovylabs.lyre.config.LyreBanner;
-import com.github.groovylabs.lyre.config.LyreProperties;
-import com.github.groovylabs.lyre.engine.APIx.APIx;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.StringUtils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Map;
 
 /**
- * Lyre class - Spring Boot Application responsible to configure and start a sandbox servlet container
- * which contains a Jersey 2 application.
- * <p>
+ * DockerConfiguration class - It's responsible to set default configuration when application is started by Docker.
  * It uses a set of {@link LyreProperties} configuration properties.
  *
- * @see EnableLyre
  */
-
-@SpringBootApplication
+@Configuration
+@Profile("docker")
 @EnableConfigurationProperties(LyreProperties.class)
-public class Lyre {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Lyre.class);
+@EnableAutoConfiguration
+public class DockerConfiguration {
 
     @Autowired
     private LyreProperties lyreProperties;
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Lyre.class);
-        app.setMainApplicationClass(Lyre.class);
-        app.setBanner(new LyreBanner());
-        app.run(args);
-    }
-
     @Bean
-    @Profile("!docker")
-    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+    public EmbeddedServletContainerCustomizer embeddedServletContainerTest() {
         return (container -> {
+
+            lyreProperties.setPort(8080);
 
             container.setContextPath(lyreProperties.getContextPath());
 
@@ -90,26 +69,6 @@ public class Lyre {
 
             container.setPort(lyreProperties.getPort());
         });
-    }
-
-    @Bean
-    public ServletRegistrationBean jerseyServletRegistration(
-        JerseyProperties jerseyProperties, ResourceConfig config) {
-
-        ServletRegistrationBean registration = new ServletRegistrationBean(
-            new ServletContainer(config));
-
-        for (Map.Entry<String, String> entry : jerseyProperties.getInit().entrySet()) {
-            registration.addInitParameter(entry.getKey(), entry.getValue());
-        }
-
-        registration.addUrlMappings("/" +
-            (StringUtils.isEmpty(lyreProperties.getApplicationPath()) ? "api" : lyreProperties.getApplicationPath())
-            + "/*");
-        registration.setName(APIx.class.getName());
-        registration.setLoadOnStartup(1);
-        return registration;
-
     }
 
 }
